e280ef80b3204764a5964c22c657a13351faf0b9
kamlesh.m <kamlesh@riva.co>
2015-09-29 13:01:23 +0530

    Using CDN now

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -1,12 +1,10 @@
 /**
  * Created by kamlesh.m on 11-Aug-15.
  **/
-var appController;
-
 require([
     "app/AppController",
     "dojo/domReady!"
 ], function (AppController) {
-    appController = new AppController(document.getElementById("bodyDiv"));
+    var appController = new AppController(document.getElementById("bodyDiv"));
 });
 
